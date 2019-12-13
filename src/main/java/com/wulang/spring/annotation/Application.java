ec7e18e4f1d39aa7b353554b18ecfa7eb8ec5cfa
wulang <1359208000@qq.com>
2019-12-13 10:08:29 +0800

    增加注解测试

--- src/main/java/com/wulang/spring/annotation/Application.java
+++ src/main/java/com/wulang/spring/annotation/Application.java
@@ -1,10 +1,14 @@
 package com.wulang.spring.annotation;
 
 import com.wulang.spring.annotation.beans.Dog;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Hello world!
@@ -32,5 +36,12 @@ public class Application
         * */
         Dog dog=(Dog) applicationContext.getBean("dog");
         dog.eat();
+    }
+
+    @Test
+    public void annotationTest(){
+        // 会查找类的注解上的注解，一直往上找
+        boolean result = AnnotatedElementUtils.isAnnotated(Dog.class, Component.class.getName());
+        System.out.println(result);
     }
 }
--- src/main/java/com/wulang/spring/annotation/beans/Dog.java
+++ src/main/java/com/wulang/spring/annotation/beans/Dog.java
@@ -1,8 +1,9 @@
 package com.wulang.spring.annotation.beans;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Dog {
     public void eat(){
         System.out.println("eating");
